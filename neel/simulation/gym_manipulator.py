3b5094fab50c61bbf69c16a8d84328ae5aa76d06
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2025-11-19 08:32:05 -0800

    Initial commit

---
+++ neel/simulation/gym_manipulator.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+"""
+
+Created by Indraneel on 11/19/2025
+
+Gym environment for mujoco
+
+"""
+
+from lerobot.configs import parser
+
+@parser.wrap()
+def main() -> None:
+    """ Main entry """
+    
+
+
+if __name__ == "__main__":
+    main()
