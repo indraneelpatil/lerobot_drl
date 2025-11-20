718c97734b17a54c4293bd9f94a35b442e6d44a6
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2025-11-20 08:22:00 -0800

    Created robot environment using gym.make

--- neel/simulation/gym_manipulator.py
+++ neel/simulation/gym_manipulator.py
@@ -7,11 +7,67 @@ Gym environment for mujoco
 
 """
 
+import logging
+from dataclasses import dataclass
+from typing import Any
+
+import gymnasium as gym
 from lerobot.configs import parser
+from lerobot.envs.configs import HILSerlRobotEnvConfig
+
+logging.basicConfig(level=logging.INFO)
+
+@dataclass
+class DatasetConfig:
+    """ Configuration for dataset creation and management."""
+
+    repo_id: str
+    task: str
+    root: str | None = None
+    num_episodes_to_record: int = 5
+    replay_episode: int | None = None
+    push_to_hub: bool = False
+
+
+@dataclass
+class GymManipulatorConfig:
+
+    env: HILSerlRobotEnvConfig
+    dataset: DatasetConfig
+    mode: str | None = None
+    device: str = "cpu"
+
+def make_robot_env(cfg: HILSerlRobotEnvConfig) -> tuple[gym.Env, Any]:
+    """Create robot environment
+    
+    """
+    if cfg.name == "gym_hil":
+        assert cfg.robot is None and cfg.teleop is None, "GymHIL environment does not support robot or teleop"
+        import gym_hil
+
+        # Extract gripper settings
+        use_gripper = cfg.processor.gripper.use_gripper if cfg.processor.gripper is not None else True
+        gripper_penalty = cfg.processor.gripper.gripper_penalty if cfg.processor.gripper is not None else 0.0
+
+        env = gym.make(
+            f"gym_hil/{cfg.task}",
+            image_obs=True,
+            render_mode="human",
+            use_gripper=use_gripper,
+            gripper_penalty=gripper_penalty,
+        )
+
+        return env, None
+
+    # TODO Real robot environment
+    raise NotImplementedError("Real robot environment not implemented yet")
+
+
 
 @parser.wrap()
-def main() -> None:
+def main(cfg: GymManipulatorConfig) -> None:
     """ Main entry """
+    env, teleop_device = make_robot_env(cfg.env)
     
 
 
