341dc19152dbfb9516febfbc258a594b4a4b669f
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2025-11-21 08:30:59 -0800

    Finished action processors and started control loop

--- neel/simulation/gym_manipulator.py
+++ neel/simulation/gym_manipulator.py
@@ -14,6 +14,18 @@ from typing import Any
 import gymnasium as gym
 from lerobot.configs import parser
 from lerobot.envs.configs import HILSerlRobotEnvConfig
+from lerobot.teleoperators.teleoperator import Teleoperator
+from lerobot.processor import (
+    AddBatchDimensionProcessorStep,
+    DataProcessorPipeline,
+    DeviceProcessorStep,
+    EnvTransition,
+    InterventionActionProcessorStep,
+    Torch2NumpyActionProcessorStep,
+    Numpy2TorchActionProcessorStep,
+    VanillaObservationProcessorStep,
+)
+from lerobot.processor.converters import identity_transition
 
 logging.basicConfig(level=logging.INFO)
 
@@ -63,11 +75,73 @@ def make_robot_env(cfg: HILSerlRobotEnvConfig) -> tuple[gym.Env, Any]:
     raise NotImplementedError("Real robot environment not implemented yet")
 
 
+def make_processors(
+        env: gym.Env, teleop_device: Teleoperator | None, cfg: HILSerlRobotEnvConfig, device: str ="cpu"
+) -> tuple[DataProcessorPipeline[EnvTransition, EnvTransition], DataProcessorPipeline[EnvTransition, EnvTransition]]:
+    """Create action processors"""
+    terminate_on_success = (
+        cfg.processor.reset.terminate_on_success if cfg.processor.reset is not None else True
+    )
+
+    if cfg.name == "gym_hil":
+        action_pipeline_steps = [
+            InterventionActionProcessorStep(terminate_on_success=terminate_on_success),
+            Torch2NumpyActionProcessorStep()
+        ]
+
+        env_pipeline_steps = [
+            Numpy2TorchActionProcessorStep(),
+            VanillaObservationProcessorStep(),
+            AddBatchDimensionProcessorStep(),
+            DeviceProcessorStep(device=device)
+        ]
+    
+        return DataProcessorPipeline(
+            steps=env_pipeline_steps, to_transition=identity_transition, to_output=identity_transition
+        ), DataProcessorPipeline(steps=action_pipeline_steps, to_transition=identity_transition, to_output=identity_transition)
+
+    raise NotImplementedError("Real robot processors not implemented")
+
+
+def control_loop(env: gym.Env, 
+                 env_processor: DataProcessorPipeline[EnvTransition, EnvTransition],
+                 action_processor: DataProcessorPipeline[EnvTransition, EnvTransition],
+                 teleop_device: Teleoperator,
+                 cfg: GymManipulatorConfig
+                 ) -> None:
+    """ Main control loop for environment interaction"""
+    dt = 1.0 / cfg.env.fps
+
+    print(f"Starting control loop at {cfg.env.fps} FPS")
+    print("Controls:")
+    print("- Use teleop device for intervention")
+    print("- When not intervening, robot will stay still")
+    print("- Press Ctrl+C to exit")
+
+    # Reset environment and processors
+    obs, info = env.reset()
+    complimentary_data = (
+        {"raw_joint_positions": info.pop("raw_joint_positions")} if "raw_joint_positions" in info else {}
+    )
+    env_processor.reset()
+    action_processor.reset()
+
+    pass
 
 @parser.wrap()
 def main(cfg: GymManipulatorConfig) -> None:
     """ Main entry """
     env, teleop_device = make_robot_env(cfg.env)
+    env_processor, action_processor = make_processors(env, teleop_device, cfg.env, cfg.device)
+
+    print("Environment observation space:", env.observation_space)
+    print("Environment Action Space:", env.action_space)
+    print("Environment processor:", env_processor)
+    print("Action processor:", action_processor)
+
+    # TODO Implement replay mode
+
+    control_loop()
     
 
 
