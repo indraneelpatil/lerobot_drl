5b1868050a44a556d7e5170553b8c5cfeae8473f
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2025-12-16 08:35:46 -0800

    Finished lerobot connection functions

--- neel/simulation/actor.py
+++ neel/simulation/actor.py
@@ -8,6 +8,8 @@ Actor process to interact with environment with current policy and collect inter
 import os
 import logging
 from functools import lru_cache
+import grpc
+import time
 
 from lerobot.configs import parser
 from lerobot.configs.train import TrainRLServerPipelineConfig
@@ -15,6 +17,10 @@ from lerobot.utils.utils import (
     init_logging
 )
 from lerobot.rl.process import ProcessSignalHandler
+from lerobot.transport.utils import (
+    grpc_channel_options
+)
+from lerobot.transport import services_pb2, services_pb2_grpc
 
 def use_threads(cfg: TrainRLServerPipelineConfig) -> bool:
     return cfg.policy.concurrency.actor == "threads"
@@ -41,16 +47,58 @@ def actor_cli(cfg: TrainRLServerPipelineConfig):
     is_threaded = use_threads(cfg)
     shutdown_event = ProcessSignalHandler(is_threaded, display_pid=display_pid).shutdown_event
 
+    # Create the grpc server 
     learner_client, grpc_channel = learner_service_client(
         host=cfg.policy.actor_learner_config.learner_host.
         port=cfg.policy.actor_learner_config.learner_port,
     )
 
+    logging.info("[ACTOR] Establishing connection with Learner")
+    if not establish_learner_connection():
+        pass
+
+
+
+def establish_learner_connection(
+    stub: services_pb2_grpc.LearnerServiceStub,
+    shutdown_event: Event, # type: ignore
+    attempts: int = 30,
+):
+    """
+        Establish a connection with learner
+    """
+    for _ in range(attempts):
+        if shutdown_event.is_set():
+            logging.info("[ACTOR] Shutting down establish_learner_connection")
+            return False
+
+        # Force a connection attempt and check state
+        try:
+            logging.info("[ACTOR] Send ready message to Learner")
+            if stub.Ready(services_pb2.Empty()) == services_pb2.Empty():
+                return True
+        except grpc.RpcError as e:
+            logging.error(f"[ACTOR] Waiting for Learner to be ready... {e}")
+            time.sleep(2)
+    return False 
+
 # Run function only once and cache the result
 @lru_cache(max_size=1)
 def learner_service_client(
-):
-    pass
+    host: str = "127.0.0.1",
+    port: int = 50051,
+) -> tuple[services_pb2_grpc.LearnerServiceStub, grpc.Channel]:
+    """
+    Returns a client for the learner service
+    """
+    channel = grpc.insecure_channel(
+       f"{host}:{port}",
+       grpc_channel_options() 
+    )
+    stub = services_pb2_grpc.LearnerServiceStub(channel)
+    logging.info("[ACTOR] Learner service client created")
+    return stub, channel
+
 
 
 if __name__ == "__main__":
