bae7f32c2664080ba229388fe0fd81645a2623c9
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2025-12-31 14:48:52 -0800

    Copied over processors and control loop as it is

--- neel/simulation/isaac_gym_manipulator.py
+++ neel/simulation/isaac_gym_manipulator.py
@@ -127,36 +127,241 @@ def make_robot_env(cfg: HILSerlRobotEnvConfig, device: str) -> tuple[gym.Env, An
     raise NotImplementedError("Real robot environment not implemented yet")
 
 
-def control_loop(env: gym.Env, cfg: GymManipulatorConfig):
+
+def step_env_and_process_transition(
+    env: gym.Env,
+    transition: EnvTransition,
+    action: torch.Tensor,
+    env_processor: DataProcessorPipeline[EnvTransition, EnvTransition],
+    action_processor: DataProcessorPipeline[EnvTransition, EnvTransition],
+) -> EnvTransition:
+    """
+    Execute one step with processor pipeline
+    """
+
+    # Create action transition
+    transition[TransitionKey.ACTION] = action
+    transition[TransitionKey.OBSERVATION] = (
+        env.get_raw_joint_positions() if hasattr(env, "get_raw_joint_positions") else {}
+    )
+    processed_action_transition = action_processor(transition)
+    processed_action = processed_action_transition[TransitionKey.ACTION]
+
+    obs, reward, terminated, truncated, info = env.step(processed_action)
+
+
+    reward = reward + processed_action_transition[TransitionKey.REWARD]
+    terminated = terminated or processed_action_transition[TransitionKey.DONE]
+    truncated = truncated or processed_action_transition[TransitionKey.TRUNCATED]
+    complementary_data= processed_action_transition[TransitionKey.COMPLEMENTARY_DATA]
+    new_info = processed_action_transition[TransitionKey.INFO].copy()
+    new_info.update(info)
+
+    new_transition = create_transition(
+        observation=obs,
+        action=processed_action,
+        reward=reward,
+        done=terminated,
+        truncated=truncated,
+        info=new_info,
+        complementary_data=complementary_data
+    )
+    new_transition = env_processor(new_transition)
+
+    return new_transition
+
+
+def control_loop(env: gym.Env,
+                 env_processor: DataProcessorPipeline[EnvTransition, EnvTransition],
+                 action_processor: DataProcessorPipeline[EnvTransition, EnvTransition],
+                cfg: GymManipulatorConfig):
     """ Main control loop for environment interaction"""
     dt = 1.0 / cfg.env.fps
 
     print(f"Starting control loop at {cfg.env.fps} FPS")
+    print("Controls:")
+    print("- Use teleop device for intervenion")
+    print("- When not intervening, robot will stay still")
+    print("- Press Ctrl+C to exit")
 
     # Reset environment
     obs, info = env.reset()
+    complimentary_data = (
+        {"raw_joint_positions": info.pop("raw_joint_positions")} if "raw_joint_positions" in info else {}
+    )
+    env_processor.reset()
+    action_processor.reset()
 
+    # Process initial observation
+    transition = create_transition(observation=obs, info=info, complementary_data=complimentary_data)
+    # print(f"Observation is {obs}")
+    transition = env_processor(data=transition)
+
+    # Check gripper
+    use_gripper = cfg.env.processor.gripper.use_gripper if cfg.env.processor.gripper is not None else True
+
+    dataset = None
+    if cfg.mode == "record":
+        #action_features = teleop_device.action_features
+        features = {
+            ACTION: {"dtype": "float32", "shape": (4,), "names": None},
+            REWARD: {"dtype": "float32", "shape": (1,), "names": None},
+            DONE: {"dtype": "bool", "shape": (1,), "names": None},
+        }
+        if use_gripper:
+            features["complementary_info.discrete_penalty"] = {
+                "dtype": "float32",
+                "shape": (1,),
+                "names": ["discrete_penalty"],
+            }
+
+        for key, value in transition[TransitionKey.OBSERVATION].items():
+            if key == OBS_STATE:
+                features[key] = {
+                    "dtype": "float32",
+                    "shape": value.squeeze(0).shape,
+                    "names": None
+                }
+            if "image" in key:
+                features[key] = {
+                    "dtype": "video",
+                    "shape": value.squeeze(0).shape,
+                    "names": ["channels", "height", "width"] 
+                }
+
+        # Create dataset
+        dataset = LeRobotDataset.create(
+            cfg.dataset.repo_id,
+            cfg.env.fps,
+            root=cfg.dataset.root,
+            use_videos=True,
+            image_writer_threads=4,
+            image_writer_processes=0,
+            features=features
+        )
+
+    episode_idx = 0
+    episode_step = 0
+    episode_start_time = time.perf_counter()
+
+    print("############################## Starting control loop")
     # simulate environment
-    while simulation_app.is_running():
+    while episode_idx < cfg.dataset.num_episodes_to_record and simulation_app.is_running():
         step_start_time = time.perf_counter()
-        actions = None
+        
+        # Create a neutral action 
+        neutral_action = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float32)
+        if use_gripper:
+            neutral_action = torch.cat([neutral_action, torch.tensor([1.0])]) # Gripper stay
 
-        if actions is None:
-            env.render()
-        else:
-            env.step(actions)
+        # Use the new step function
+        transition = step_env_and_process_transition(
+            env=env,
+            transition=transition,
+            action=neutral_action,
+            env_processor=env_processor,
+            action_processor=action_processor,
+        )
+        terminated = transition.get(TransitionKey.DONE, False)
+        truncated = transition.get(TransitionKey.TRUNCATED, False)
+
+        if cfg.mode == "record":
+            observations = {
+                k: v.squeeze(0).cpu() 
+                for k,v in transition[TransitionKey.OBSERVATION].items()
+                if isinstance(v, torch.Tensor)
+            }
+            # Use teleop_action if available, otherwise use the action from the transition
+            action_to_record = transition[TransitionKey.INFO].get(
+                "teleop_action", transition[TransitionKey.ACTION]
+            )
+            frame = {
+                **observations,
+                ACTION: action_to_record.cpu() if isinstance(action_to_record, torch.Tensor) else action_to_record.astype(np.float32),
+                REWARD: np.array([transition[TransitionKey.REWARD]], dtype=np.float32),
+                DONE: np.array([terminated or truncated], dtype=bool)
+            }
+            if use_gripper:
+                discrete_penalty = transition[TransitionKey.INFO].get("discrete_penalty", 0.0)
+                frame["complementary_info.discrete_penalty"] = np.array([discrete_penalty], dtype=np.float32)
+            
+            if dataset is not None:
+                frame["task"] = cfg.dataset.task
+                dataset.add_frame(frame)
+
+        episode_step += 1
+
+        # Handle termination
+        if terminated or truncated:
+            episode_time = time.perf_counter() - episode_start_time
+            print(
+                f"Episode ended after {episode_step} steps in {episode_time:.1f}s with reward {transition[TransitionKey.REWARD]}"            
+            )
+            episode_step = 0
+            episode_idx += 1
+
+            if dataset is not None:
+                if transition[TransitionKey.INFO].get("rerecord_episode", False):
+                    print(f"Re-recording episode {episode_idx}")
+                    dataset.clear_episode_buffer()
+                    episode_idx -= 1
+                else:
+                    print(f"Saving episode {episode_idx}")
+                    dataset.save_episode()
+
+            # Reset for new episode
+            obs, info = env.reset()
+            env_processor.reset()
+            action_processor.reset()
+
+
+            transition = create_transition(observation=obs, info=info)
+            transition = env_processor(transition)
 
         # Maintain fps timing
         busy_wait(dt - (time.perf_counter() - step_start_time))
+
+    # TODO Push to hub
         
 
+def make_processors(
+        env: gym.Env, teleop_device: Teleoperator | None, cfg: HILSerlRobotEnvConfig, device: str ="cpu"
+) -> tuple[DataProcessorPipeline[EnvTransition, EnvTransition], DataProcessorPipeline[EnvTransition, EnvTransition]]:
+    """Create action processors"""
+    terminate_on_success = (
+        cfg.processor.reset.terminate_on_success if cfg.processor.reset is not None else True
+    )
+
+    if cfg.name == "gym_isaac_sim_hil":
+        action_pipeline_steps = [
+            InterventionActionProcessorStep(terminate_on_success=terminate_on_success),
+        ]
+
+        env_pipeline_steps = [
+            Numpy2TorchActionProcessorStep(),
+            VanillaObservationProcessorStep(),
+            AddBatchDimensionProcessorStep(),
+            DeviceProcessorStep(device=device)
+        ]
+    
+        return DataProcessorPipeline(
+            steps=env_pipeline_steps, to_transition=identity_transition, to_output=identity_transition
+        ), DataProcessorPipeline(steps=action_pipeline_steps, to_transition=identity_transition, to_output=identity_transition)
+
+    raise NotImplementedError("Real robot processors not implemented")
 
 @parser.wrap()
 def main(cfg: GymManipulatorConfig) -> None:
     """ Main entry """
     env, teleop_device = make_robot_env(cfg.env, cfg.device)
+    env_processor, action_processor = make_processors(env, teleop_device, cfg.env, cfg.device)
 
-    control_loop(env,cfg)
+    print("Environment observation space:", env.observation_space)
+    print("Environment Action Space:", env.action_space)
+    print("Environment processor:", env_processor)
+    print("Action processor:", action_processor)
+
+    control_loop(env, env_processor, action_processor, cfg)
 
     # Close the simulator
     simulation_app.close()
