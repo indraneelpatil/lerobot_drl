7fc42b9e3f740f830652ed0259557a881eebb311
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2026-01-28 06:56:12 -0800

    Dataset view image script works

--- neel/scripts/dataset_view_image.py
+++ neel/scripts/dataset_view_image.py
@@ -3,16 +3,21 @@
 Docstring for lerobot_drl.neel.scripts.dataset_view_image
 """
 from datasets import load_dataset
+from lerobot.datasets.lerobot_dataset import LeRobotDataset
 import matplotlib.pyplot as plt
 
-ds = load_dataset("indraneelpatil/isaac_sim_pick_lift_sim4", split="train")
+#ds = LeRobotDataset(repo_id="indraneelpatil/isaac_sim_pick_lift_sim4")
+ds = LeRobotDataset("/home/neel/.cache/huggingface/lerobot/indraneelpatil/isaac_sim_pick_lift_sim5")
 
-sample = ds[0]
+sample = ds[120]
 
 print(sample)
 
 front = sample["observation.images.front"]
 wrist = sample["observation.images.wrist"]
+
+front = front.permute(1, 2, 0).cpu().numpy()
+wrist = wrist.permute(1, 2, 0).cpu().numpy()
 
 plt.figure(figsize=(10,4))
 
