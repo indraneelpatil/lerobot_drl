3595567db5558026cb1fdfd7f63fbeca71660f72
Indraneel Patil <indraneelrajeevpatil@gmail.com>
2026-03-02 08:17:13 -0800

    Finished writing image layers

--- neel/policies/sac/modeling_sac.py
+++ neel/policies/sac/modeling_sac.py
@@ -6,6 +6,7 @@ Reimplementation of the Soft Actor Critic Policy
 """
 
 import torch.nn as nn
+import torch
 
 from lerobot.policies.pretrained import PreTrainedPolicy
 from lerobot.utils.constants import ACTION, OBS_ENV_STATE, OBS_STATE
@@ -42,6 +43,104 @@ class SACPolicy(
         )
 
 
+class PretrainedImageEncoder(nn.Module):
+    def __init__(self, config: SACConfig):
+        super().__init__()
+
+        self.image_enc_layers, self.image_enc_out_shape = self._load_pretrained_vision_encoder(config)
+
+    def _load_pretrained_vision_encoder(self, config: SACConfig):
+        """Set up CNN encoder"""
+        from transformers import AutoModel
+
+        self.image_enc_layers = AutoModel.from_pretrained(config.vision_encoder_name, trust_remote_code=True)
+
+        if hasattr(self.image_enc_layers.config, "hidden_sizes"):
+            self.image_enc_out_shape = self.image_enc_layers.config.hidden_sizes[-1] # Last channel dimension
+        elif hasattr(self.image_enc_layers, "fc"):
+            self.image_enc_out_shape = self.image_enc_layers.fc.in_features
+        else:
+            raise ValueError("Unsupported vision encoder architecture, make sure you are using a CNN")
+        return self.image_enc_layers, self.image_enc_out_shape
+
+    def forward(self, x):
+        enc_feat = self.image_enc_layers(x).last_hidden_state
+        return enc_feat
+    
+class DefaultImageEncoder(nn.Module):
+    def __init__(self, config: SACConfig):
+        super().__init__()
+        image_key = next(key for key in config.input_features if is_image_feature(key))
+        self.image_enc_layers = nn.Sequential(
+            nn.Conv2d(
+                in_channels=config.input_features[image_key].shape[0],
+                out_channels=config.image_encoder_hidden_dim,
+                kernel_size=7,
+                stride=2,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=config.image_encoder_hidden_dim,
+                out_channels=config.image_encoder_hidden_dim,
+                kernel_size=5,
+                stride=2,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=config.image_encoder_hidden_dim,
+                out_channels=config.image_encoder_hidden_dim,
+                kernel_size=3,
+                stride=2,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=config.image_encoder_hidden_dim,
+                out_channels=config.image_encoder_hidden_dim,
+                kernel_size=3,
+                stride=2,
+            ),
+            nn.ReLU()
+        )
+    def forward(self, x):
+        x = self.image_enc_layers(x)
+        return x
+
+def freeze_image_encoder(image_encoder: nn.Module):
+    """Freeze all parameters in the encoder"""
+    for param in image_encoder.parameters():
+        param.requires_grad = False
+
+
+class SpatialLearnedEmbeddings(nn.Module):
+    def __init__(self, height, width, channel, num_features=8):
+        """
+        Pytorch implementation of learned spatial embeddings
+        """
+        super().__init__()
+        self.height = height
+        self.width = width
+        self.channel = channel
+        self.num_features = num_features
+
+        self.kernel = nn.Parameter(torch.empty(channel, height, width, num_features))
+
+        nn.init.kaiming_normal_(self.kernel, mode="fan_in", nonlinearity="linear")
+
+    def forward(self, features):
+        """
+        Forward pass for spatial embedding
+        """
+        features_expanded = features.unsqueeze(-1)
+        kernel_expanded = self.kernel.unsqueeze(0)
+
+        # Element wise multiplication and spatial reduction
+        output = (features_expanded * kernel_expanded).sum(dim=(2,3))
+
+        # Reshape to combine channel and feature dimensions
+        output = output.view(output.size(0), -1)
+
+        return output
+
 class SACObservationEncoder(nn.Module):
     """Encode image and/or state vector observations"""
 
@@ -60,6 +159,34 @@ class SACObservationEncoder(nn.Module):
             self.image_encoder = PretrainedImageEncoder(self.config)
         else:
             self.image_encoder = DefaultImageEncoder(self.config)
+        
+        if self.config.freeze_vision_encoder:
+            freeze_image_encoder(self.image_encoder)
+        
+        dummy = torch.zeros(1, *self.config.input_features[self.image_keys[0]].shape)
+        with torch.no_grad():
+            _, channels, height, width = self.image_encoder(dummy).shape
+        
+        self.spatial_embeddings = nn.ModuleDict()
+        self.post_encoders = nn.ModuleDict()
+
+        for key in self.image_keys:
+            name = key.replace(".", "_")
+            self.spatial_embeddings[name] = SpatialLearnedEmbeddings(
+                height=height,
+                width=width,
+                channel=channels,
+                num_features=self.config.image_embedding_pooling_dim,
+            )
+            self.post_encoders[name] = nn.Sequential(
+                nn.Dropout(0.1),
+                nn.Linear(
+                    in_features=channels * self.config.image_embedding_pooling_dim,
+                    out_features=self.config.latent_dim,
+                ),
+                nn.LayerNorm(normalized_shape=self.config.latent_dim),
+                nn.Tanh()
+            )
 
 
 
